e1e140f168972a0cad957e698dd3aa97f4a913f2
Mel-JAW <m.acuna.1@estudiantec.cr>
2026-06-15 23:30:07 -0600

    Modificada la función del botón "Tiempo de gracia en minutos" para abrir el submenú correspondiente en la configuración del sistema de parqueo.

--- Menu.py
+++ Menu.py
@@ -55,7 +55,7 @@ def abrirSubmenuConfiguracion(pVentana,pEstacionamiento):
     ventana.geometry("500x400")
     Label(ventana,text="CONFIGURACIÓN",font=("Century Gothic",14,"bold")).pack(pady=15)
     Button(ventana,text="1.Tamaño del estacionamiento",font=("Century Gothic",12,"bold"),width=35,command=lambda:abrirTamannoEstacionamiento(ventana,pEstacionamiento)).pack(pady=5)
-    Button(ventana,text="2.Tiempo de gracia en minutos",font=("Century Gothic",12,"bold"),width=35,command=lambda:ejecutarFuncionPendiente("tiempoGracia")).pack(pady=5)
+    Button(ventana,text="2.Tiempo de gracia en minutos",font=("Century Gothic",12,"bold"),width=35,command=lambda:abrirTiempoGracia(ventana,pEstacionamiento)).pack(pady=5)
     Button(ventana,text="3.Modificar monto por hora",font=("Century Gothic",12,"bold"),width=35,command=lambda:ejecutarFuncionPendiente("modificarMontoHora")).pack(pady=5)
     Button(ventana,text="4.Regresar",font=("Century Gothic",12,"bold"),width=35,command=lambda:regresarMenuPrincipal(pVentana,ventana)).pack(pady=5)
 
